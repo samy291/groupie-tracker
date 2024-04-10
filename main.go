a7f4d2e28a39c6a424d2da9d5211a7b2f27eae7c
jsamy <samy.joualla@ynov.com>
2024-04-11 01:57:22 +0200

    Feat: Adding files

--- main.go
+++ main.go
@@ -1,27 +1,49 @@
 package main
 
 import (
-    "fmt"
-    "html/template"
-    "log"
-    "net/http"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
-    t, err := template.ParseFiles("./templates/accueil.html")
-    if err != nil {
-        log.Fatal(err)
-    }
-    t.Execute(w, nil) 
+	t, err := template.ParseFiles("./templates/accueil.html")
+	if err != nil {
+		log.Printf("Error parsing template: %v", err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	err = t.Execute(w, nil)
+	if err != nil {
+		log.Printf("Error executing template: %v", err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+	}
+}
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+
+	fmt.Printf("Email: %s\nPassword: %s\n", email, password)
 }
 
 func main() {
-    http.HandleFunc("/", Home)
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/login", Login)
 
-    fs := http.FileServer(http.Dir("static"))
-    http.Handle("/static/", http.StripPrefix("/static/", fs))
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-    fmt.Println("The server is running on port :8080")
-    http.ListenAndServe(":8080", nil)
-    
+	fmt.Println("The server is running on port :8081")
+	err := http.ListenAndServe(":8081", nil)
+	if err != nil {
+		log.Fatalf("Error starting server: %v", err)
+	}
 }
